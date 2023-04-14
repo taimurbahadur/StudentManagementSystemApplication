f04f4164281706661e6083f5e7e60faeea9483a0
taimurbahadur <taimurbahadur@gmail.com>
2023-04-14 12:21:02 +0500

    Created DTOs and Mapper for two extra entities for custom joint tables which has name StudentHasCourse and CourseHasAssessment respectively.

--- src/main/java/com/intellicode/studentmanagementsystemapplication/service/StudentService.java
+++ src/main/java/com/intellicode/studentmanagementsystemapplication/service/StudentService.java
@@ -1,12 +1,8 @@
 package com.intellicode.studentmanagementsystemapplication.service;
 
-import com.intellicode.studentmanagementsystemapplication.entity.AddressEntity;
 import com.intellicode.studentmanagementsystemapplication.entity.StudentEntity;
-import com.intellicode.studentmanagementsystemapplication.mapper.AddressMapper;
-import com.intellicode.studentmanagementsystemapplication.mapper.StudentMapper;
 import com.intellicode.studentmanagementsystemapplication.repository.CourseRepository;
 import com.intellicode.studentmanagementsystemapplication.repository.StudentRepository;
-import com.intellicode.studentmanagementsystemapplication.dto.StudentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,27 +14,10 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-    @Autowired
-    private CourseRepository courseRepository;
-
-//    @Autowired
-//    private StudentMapper studentMapper;
-
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-
-//    public StudentDto createStudent(StudentDto studentDto){
-//
-//        //Convert DTO into Entity
-//        StudentEntity studentEntity = mapToEntity(studentDto);
-//        StudentEntity newStudent = studentRepository.save(studentEntity);
-//
-//        //Convert Entity into DTO
-//        StudentDto studentResponse = mapToDTO(newStudent);
-//        return studentResponse;
-//    }
 
     public List<StudentEntity> getAllStudents()
     {
@@ -59,26 +38,5 @@ public class StudentService {
         studentEntity.setDeleted(true);
         studentRepository.save(studentEntity);
     }
-
-//    public StudentDto saveStudent(StudentDto studentDto){
-//        StudentEntity studentEntity = new StudentEntity();
-//        StudentMapper studentMapper = new StudentMapper();
-//
-//        studentEntity = studentMapper.mapToEntity(studentDto);
-//
-//
-//        AddressEntity addressEntity =new AddressEntity();
-//        addressEntity.setId(studentDto.get);
-//        AddressMapper addressMapper = new AddressMapper();
-//
-//        addressMapper.mapToEntity(studentEntity);
-//
-//        studentRepository.save(studentEntity);
-//
-//        return studentDto;
-//    }
-
-
-
 
 }
